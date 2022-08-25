ceda46fd2f9313ae8dc91b0d0973c38e0e23a08b
Mirko Sertic <m.sertic@thalia.de>
2022-08-25 11:39:28 +0200

    Templated build

--- downloaddata.sh
+++ downloaddata.sh
@@ -10,10 +10,11 @@ echo "<ul>" > $INDEXHTML
 
 while IFS="," read -r col1 col2 col3 col4
 do
-  export INDEXID="$col1"
-  export INDEXNAME="$col2"
-  export INDEXCURRENCY="$col3"
-  export INDEXVARIANT="$col4"
+  echo "$col1 $col2 $col3 $col4"
+  export INDEXID=$col1
+  export INDEXNAME=$col2
+  export INDEXCURRENCY=$col3
+  export INDEXVARIANT=$col4
   curl -o ./web/$INDEXID.json "https://app2.msci.com/products/service/index/indexmaster/getLevelDataForGraph?currency_symbol=$INDEXCURRENCY&index_variant=NETR&start_date=$INDEXSTART&end_date=$INDEXEND&data_frequency=DAILY&index_codes=$INDEXID"
   envsubst < template.html > web/$INDEXID.html
   echo "  <li><a href=\"$INDEXID.html\">${INDEXID} ${INDEXNAME} (${INDEXCURRENCY}/${INDEXVARIANT})</a></li>" >> $INDEXHTML
