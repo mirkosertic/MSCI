d38e58af8ad849554f1d14546c5bf0a0f5a26d33
Mirko Sertic <m.sertic@thalia.de>
2022-08-25 11:00:42 +0200

    Templated build

--- downloaddata.sh
+++ downloaddata.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-START=`date '+%Y%m%d' --date="4 years ago"`
-NOW=`date '+%Y%m%d'`
-echo $START
-echo $NOW
-curl -o ./web/733339.json "https://app2.msci.com/products/service/index/indexmaster/getLevelDataForGraph?currency_symbol=EUR&index_variant=NETR&start_date=$START&end_date=$NOW&data_frequency=DAILY&index_codes=733339"
+export INDEXSTART=`date '+%Y%m%d' --date="4 years ago"`
+export INDEXEND=`date '+%Y%m%d'`
+export INDEXID="733339"
+export INDEXNAME="MSCI World Visualization"
+export INDEXCURRENCY="EUR"
+export INDEXVARIANT="NETR"
+curl -o ./web/$INDEXID.json "https://app2.msci.com/products/service/index/indexmaster/getLevelDataForGraph?currency_symbol=$INDEXCURRENCY&index_variant=NETR&start_date=$INDEXSTART&end_date=$INDEXEND&data_frequency=DAILY&index_codes=$INDEXID"
+envsubst < template.html > web/$INDEXID.html
